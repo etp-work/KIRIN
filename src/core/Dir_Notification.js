2c9bbb27d23089205436218d7e2c7befaba41ec2
zuohao <leftstick@qq.com>
2013-11-27 16:14:47 +0800

    remove debug msg from notification

--- src/core/Dir_Notification.js
+++ src/core/Dir_Notification.js
@@ -5,7 +5,7 @@ mainApp.directive('kiNotification', ['notificationMgr',
     function(notificationMgr) {
         return {
             restrict: 'E',
-            template: '<div ng-repeat="notification in notifications"><div class="alert alert-dismissable" ng-class="notification.type" ng-show="$last"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong><span class="glyphicon" ng-class="notification.title"></span></strong>&nbsp;&nbsp;{{$index}}<span ng-bind="notification.msg"></span></div></div>',
+            template: '<div ng-repeat="notification in notifications" class="notify"><div class="alert alert-dismissable" ng-class="notification.type" ng-show="$last"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong><span class="glyphicon" ng-class="notification.title"></span></strong>&nbsp;&nbsp;<span ng-bind="notification.msg"></span></div></div>',
             link: function (scope, element, attrs, controller) {
                 scope.notifications = notificationMgr.getNotifications();
             }
