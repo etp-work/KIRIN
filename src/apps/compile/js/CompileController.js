c74f6319836e1135f246467053a93a9e5f8718b7
zuohao <leftstick@qq.com>
2013-11-27 18:51:53 +0800

    add inline annotation for compile

--- src/apps/compile/js/CompileController.js
+++ src/apps/compile/js/CompileController.js
@@ -1,6 +1,8 @@
 /*
  * The controller for compile application
  */
-mainApp.controller("CompileController", function($scope) {
-	// TODO: Add logic for compile application
-});
+mainApp.controller("CompileController", ['$scope',
+    function($scope) {
+        // TODO: Add logic for compile application
+    }
+]);
