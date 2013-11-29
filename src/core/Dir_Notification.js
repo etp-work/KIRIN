0c41b2a5f2c7c183ebbce9e9fd33a73e1c4ed117
zuohao <leftstick@qq.com>
2013-11-29 14:41:03 +0800

    fix bug that notification will not be poped up when the last one is removed

--- src/core/Dir_Notification.js
+++ src/core/Dir_Notification.js
@@ -5,9 +5,13 @@ mainApp.directive('kiNotification', ['notificationMgr',
     function(notificationMgr) {
         return {
             restrict: 'E',
-            template: '<div ng-repeat="notification in notifications" class="notify"><div class="alert alert-dismissable" ng-class="notification.type" ng-show="$last"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong><span class="glyphicon" ng-class="notification.title"></span></strong>&nbsp;&nbsp;<span ng-bind="notification.msg"></span></div></div>',
+            template: '<div ng-repeat="notification in notifications" class="notify"><div class="alert alert-dismissable" ng-class="notification.type" ng-show="$last"><button type="button" class="close" ng-click="close(notification)" aria-hidden="true">&times;</button><strong><span class="glyphicon" ng-class="notification.title"></span></strong>&nbsp;&nbsp;<span ng-bind="notification.msg"></span></div></div>',
             link: function (scope, element, attrs, controller) {
                 scope.notifications = notificationMgr.getNotifications();
+                
+                scope.close = function(notification){
+                    notificationMgr.removeNotification(notification.id);
+                };
             }
         }
    }]
