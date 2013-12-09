3c3d4bd84a99f679041ecbacffe7af10b0c290db
zuohao <leftstick@qq.com>
2013-12-09 10:37:25 +0800

    should not enable check option because it means only check syntax without evaluation

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,10 +38,6 @@ module.exports = function(grunt) {
             }
         },
         sass: {
-            options: {
-                trace: true,
-                check: true
-            },
             dev: {
                 files: [{
                     expand: true,
